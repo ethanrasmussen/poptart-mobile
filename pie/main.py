59f84f09f30a2ab8ca21bc95383de533231f8ec3
ethanrasmussen <itseraz@gmail.com>
2021-01-11 18:03:46 -0600

    update L/R front motor var names

--- pie/main.py
+++ pie/main.py
@@ -6,8 +6,8 @@ import gpiozero as gz
 
 
 # motor assignment
-L_MOTOR = gz.OutputDevice(27)
-R_MOTOR = gz.OutputDevice(17)
+FRONT_L_MOTOR = gz.OutputDevice(27)
+FRONT_R_MOTOR = gz.OutputDevice(17)
 
 # init Flask app
 app = Flask(__name__)
@@ -16,29 +16,29 @@ app = Flask(__name__)
 # forward
 @app.route('/f', methods=['GET'])
 def forward():
-    L_MOTOR.on()
-    R_MOTOR.on()
+    FRONT_L_MOTOR.on()
+    FRONT_R_MOTOR.on()
     return True
 
 # stop
 @app.route('/s', methods=['GET'])
 def stop():
-    L_MOTOR.off()
-    R_MOTOR.off()
+    FRONT_L_MOTOR.off()
+    FRONT_R_MOTOR.off()
     return True
 
 # left
 @app.route('/l', methods=['GET'])
 def left():
-    L_MOTOR.on()
-    R_MOTOR.off()
+    FRONT_L_MOTOR.on()
+    FRONT_R_MOTOR.off()
     return True
 
 # right
 @app.route('/r', methods=['GET'])
 def right():
-    L_MOTOR.off()
-    R_MOTOR.on()
+    FRONT_L_MOTOR.off()
+    FRONT_R_MOTOR.on()
     return True
 
 
